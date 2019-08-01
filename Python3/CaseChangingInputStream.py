7b8bd54040fbfd1de0ec683c6d5f1ff3c3916270
Kazuki Sawada <kazuki@6715.jp>
2019-08-01 22:30:17 +0900

    Impl: Python3

---
+++ Python3/CaseChangingInputStream.py
@@ -0,0 +1,15 @@
+from antlr4 import InputStream
+
+class CaseChangingInputStream():
+	def __init__(self, stream: InputStream, upper: bool):
+		self._stream = stream
+		self._upper = upper
+
+	def __getattr__(self, name: str):
+		return self._stream.__getattribute__(name)
+
+	def LA(self, offset: int):
+		c = self._stream.LA(offset)
+		if c <= 0:
+			return c
+		return ord(chr(c).upper() if self._upper else chr(c).lower())
--- Python3/main.py
+++ Python3/main.py
@@ -5,15 +5,22 @@ from antlr4 import *
 from MySqlLexer import MySqlLexer
 from MySqlParser import MySqlParser
 from MySqlParserListener import MySqlParserListener
+from CaseChangingInputStream import CaseChangingInputStream
 
 class Listener(MySqlParserListener):
-	def enterEveryRule(self, ctx:ParserRuleContext):
-		print(ctx)
+	def __init__(self):
+		self.depth = 0
 
-	def exitEveryRule(self, ctx:ParserRuleContext):
-		print(ctx)
+	def enterEveryRule(self, ctx):
+		print("  " * self.depth + ctx.__class__.__name__)
+		self.depth += 1
 
-parser = MySqlParser(CommonTokenStream(MySqlLexer(FileStream("../target.sql"))))
+	def exitEveryRule(self, ctx):
+		self.depth -= 1
+
+stream = FileStream("../target.sql")
+
+parser = MySqlParser(CommonTokenStream(MySqlLexer(CaseChangingInputStream(stream, True))))
 listener = Listener()
 
 ParseTreeWalker().walk(listener, parser.root())
