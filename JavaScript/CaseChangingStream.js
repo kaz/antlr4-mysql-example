2b849ea0b6daabf38eec94e0f666e62a48d33539
Kazuki Sawada <kazuki@6715.jp>
2019-08-01 22:36:33 +0900

    refactoring: JavaScript

--- JavaScript/CaseChangingStream.js
+++ JavaScript/CaseChangingStream.js
@@ -5,12 +5,12 @@
  */
 //
 
-function CaseInsensitiveInputStream(stream, upper) {
+function CaseChangingStream(stream, upper) {
 	this._stream = stream;
 	this._upper = upper;
 }
 
-CaseInsensitiveInputStream.prototype.LA = function(offset) {
+CaseChangingStream.prototype.LA = function(offset) {
 	var c = this._stream.LA(offset);
 	if (c <= 0) {
 		return c;
@@ -18,48 +18,48 @@ CaseInsensitiveInputStream.prototype.LA = function(offset) {
 	return String.fromCodePoint(c)[this._upper ? "toUpperCase" : "toLowerCase"]().codePointAt(0);
 };
 
-CaseInsensitiveInputStream.prototype.reset = function() {
+CaseChangingStream.prototype.reset = function() {
 	return this._stream.reset();
 };
 
-CaseInsensitiveInputStream.prototype.consume = function() {
+CaseChangingStream.prototype.consume = function() {
 	return this._stream.consume();
 };
 
-CaseInsensitiveInputStream.prototype.LT = function(offset) {
+CaseChangingStream.prototype.LT = function(offset) {
 	return this._stream.LT(offset);
 };
 
-CaseInsensitiveInputStream.prototype.mark = function() {
+CaseChangingStream.prototype.mark = function() {
 	return this._stream.mark();
 };
 
-CaseInsensitiveInputStream.prototype.release = function(marker) {
+CaseChangingStream.prototype.release = function(marker) {
 	return this._stream.release(marker);
 };
 
-CaseInsensitiveInputStream.prototype.seek = function(_index) {
+CaseChangingStream.prototype.seek = function(_index) {
 	return this._stream.seek(_index);
 };
 
-CaseInsensitiveInputStream.prototype.getText = function(start, stop) {
+CaseChangingStream.prototype.getText = function(start, stop) {
 	return this._stream.getText(start, stop);
 };
 
-CaseInsensitiveInputStream.prototype.toString = function() {
+CaseChangingStream.prototype.toString = function() {
 	return this._stream.toString();
 };
 
-Object.defineProperty(CaseInsensitiveInputStream.prototype, "index", {
+Object.defineProperty(CaseChangingStream.prototype, "index", {
 	get: function() {
 		return this._stream.index;
 	}
 });
 
-Object.defineProperty(CaseInsensitiveInputStream.prototype, "size", {
+Object.defineProperty(CaseChangingStream.prototype, "size", {
 	get: function() {
 		return this._stream.size;
 	}
 });
 
-exports.CaseInsensitiveInputStream = CaseInsensitiveInputStream;
+exports.CaseChangingStream = CaseChangingStream;
--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,25 +1,28 @@
 "use strict";
 
-const fs = require("fs");
-
 const antlr4 = require("antlr4");
 const {MySqlLexer} = require("./parser/MySqlLexer");
 const {MySqlParser} = require("./parser/MySqlParser");
 const {MySqlParserListener} = require("./parser/MySqlParserListener");
-const {CaseInsensitiveInputStream} = require("./CaseInsensitiveInputStream");
+const {CaseChangingStream} = require("./CaseChangingStream");
 
-let depth = 0;
-MySqlParserListener.prototype.enterEveryRule = ctx => {
-	console.log("  ".repeat(depth) + ctx.constructor.name);
-	depth++;
-};
-MySqlParserListener.prototype.exitEveryRule = ctx => {
-	depth--;
-};
+class Listener extends MySqlParserListener {
+	constructor(){
+		super();
+		this.depth = 0;
+	}
+	enterEveryRule(ctx){
+		console.log("  ".repeat(this.depth) + ctx.constructor.name);
+		this.depth += 1;
+	}
+	exitEveryRule(ctx){
+		this.depth -= 1;
+	}
+}
 
 const stream = new antlr4.FileStream("../target.sql", true);
 
-const parser = new MySqlParser(new antlr4.CommonTokenStream(new MySqlLexer(new CaseInsensitiveInputStream(stream, true))));
-const listener = new MySqlParserListener();
+const parser = new MySqlParser(new antlr4.CommonTokenStream(new MySqlLexer(new CaseChangingStream(stream, true))));
+const listener = new Listener();
 
 antlr4.tree.ParseTreeWalker.DEFAULT.walk(listener, parser.root());
